62420044db2119a40e48fa4ac02056445a6bc295
raahede <frejraahede@gmail.com>
2016-12-19 15:13:10 +0100

    Revert "Setting up transitions"
    This reverts commit d5d8d5d16a58a637f13f3bcad4167d2b4dd4fc3e.

--- js/app.js
+++ js/app.js
@@ -4,45 +4,9 @@
     accessToken: '272b121abec98bcd8650d604bb626f2e1bb52ce70ba02906db3bab3319886f3f'
   });
 
-  var Animations = {
-    data: function() {
-      return {
-        transitionName: 'slide-left'
-      };
-    },
-    methods: {
-      beforeEnter: function (el) {
-        if(this.transitionName === 'slide-left') {
-          el.style.translateX = '100%';
-        } else {
-          el.style.translateX = '-100%';
-        }
-      },
-      enter: function (el, done) {
-        Velocity(el, { translateX: '0%' }, { duration: 300 }, { complete: done });
-      },
-      leave: function (el, done) {
-        if(this.transitionName === 'slide-left') {
-          Velocity(el, { translateX: '-100%' }, { duration: 300 }, { complete: done });
-        } else {
-          Velocity(el, { translateX: '100%' }, { duration: 300 }, { complete: done });
-        }
-      }
-    },
-    watch: {
-      '$route': function(to, from) {
-        var toDepth = to.meta.depth;
-        var fromDepth = from.meta.depth;
-        this.transitionName = to.meta.depth < from.meta.depth ? 'slide-right' : 'slide-left';
-        console.log(to, from, this.transitionName);
-      }
-    }
-  };
-
   var ContentTypes = {
     name: 'ContentTypes',
     template: '#my-content-types',
-    mixins: [Animations],
     data: function() {
       return {
         contentTypes: null
@@ -59,7 +23,6 @@
   var Entries = {
     name: 'Entries',
     template: '#my-entries',
-    mixins: [Animations],
     data: function() {
       return {
         entries: null
@@ -101,7 +64,6 @@
   };
 
   var Entry = {
-    mixins: [Animations],
     data: function() {
       return {
         entry: null,
@@ -201,66 +163,36 @@
         path: '*',
         component: ContentTypes,
         name: 'contentTypes',
-        meta: { depth: 1 }
-        // children: [
-        //   {
-        //     path: '/:contentType',
-        //     name: 'entries',
-        //     component: Entries,
-        //     meta: { depth: 2 },
-        //     children: [
-        //       {
-        //         path: '/:contentType/image/:entryId',
-        //         name: 'image',
-        //         component: Image,
-        //         meta: { depth: 3 }
-        //       },
-        //       {
-        //         path: '/:contentType/author/:entryId',
-        //         name: 'author',
-        //         component: Author,
-        //         meta: { depth: 3 }
-        //       },
-        //       {
-        //         path: '/:contentType/gallery/:entryId',
-        //         name: 'gallery',
-        //         component: Gallery,
-        //         meta: { depth: 3 }
-        //       }
-        //     ]
-        //   }
-        // ]
-      },
-      {
-        path: '/:contentType',
-        name: 'entries',
-        component: Entries,
-        meta: { depth: 2 }
-      },
-      {
-        path: '/:contentType/image/:entryId',
-        name: 'image',
-        component: Image,
-        meta: { depth: 3 }
-      },
-      {
-        path: '/:contentType/author/:entryId',
-        name: 'author',
-        component: Author,
-        meta: { depth: 3 }
-      },
-      {
-        path: '/:contentType/gallery/:entryId',
-        name: 'gallery',
-        component: Gallery,
-        meta: { depth: 3 }
+        children: [
+          {
+            path: '/:contentType',
+            name: 'entries',
+            component: Entries,
+            children: [
+              {
+                path: '/:contentType/image/:entryId',
+                name: 'image',
+                component: Image
+              },
+              {
+                path: '/:contentType/author/:entryId',
+                name: 'author',
+                component: Author
+              },
+              {
+                path: '/:contentType/gallery/:entryId',
+                name: 'gallery',
+                component: Gallery
+              }
+            ]
+          }
+        ]
       }
     ]
   });
 
   new Vue({
     el: '#app',
-    mixins: [Animations],
     components: {
       'my-content-types': ContentTypes
     },
